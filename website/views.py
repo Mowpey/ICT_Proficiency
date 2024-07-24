764e65b9572a07570a39ecca92ae76c4d8d8f0dc
Dawntless <66586346+Mowpey@users.noreply.github.com>
2024-07-24 16:54:17 +0800

    added modal buttons and modified the table
    added modal buttons and put some huge changes in the table UI. Also fixed the responsiveness of the table and put decent filter UI.

--- website/views.py
+++ website/views.py
@@ -5,11 +5,17 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def showDashboard():
     return render_template("dashboard.html")
+    
 
-@views.route('/table')
-def showTable():
-    return render_template("table.html")
+@views.route('/diagnostic_table')
+def showDiagnosticTable():
+    return render_template("diagnostic_table.html")
+
+@views.route('/handson_table')
+def showHandsonTable():
+    return render_template("handson_table.html")
 
 @views.route('/history')
 def showHistory():
     return render_template("history.html")
+
