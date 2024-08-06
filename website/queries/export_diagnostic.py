b4a07527ec0d37ba3251a59c30aa296dd3b464d7
Donkey <114471039+kentobi09@users.noreply.github.com>
2024-08-06 10:48:00 +0800

    export and history

--- website/queries/export_diagnostic.py
+++ website/queries/export_diagnostic.py
@@ -7,8 +7,9 @@ export_bp = Blueprint('export', __name__)
 def export_applicant(applicant_id):
     applicant = DiagnosticResults.query.get_or_404(applicant_id)
     handson_details = handson_details = applicant.handson_results
+    assessment = assessment  = applicant.user_assessment
     # Render the CSV template
-    csv_data = render_template('applicant_export.csv', applicant=applicant,handson_details=handson_details)
+    csv_data = render_template('applicant_export.csv', applicant=applicant,handson_details=handson_details,assessment = assessment)
     
     # Create a response with CSV mimetype
     response = make_response(csv_data)
